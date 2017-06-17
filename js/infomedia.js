eeab0e192dc8bf2da2165c6c4da99b747971f043
windsor80 <windsorjunior@hotmail.com>
2017-06-17 00:58:20 -0400

    updates

--- js/infomedia.js
+++ js/infomedia.js
@@ -1,15 +1,13 @@
-var fs = require('fs');
-
-var Twitter = require('twitter');
-
-var request = require('request');
-
-var spotify = require('spotify');
-
-
 $(document).ready(function() {
 
-$(document).find(".botao").hide();
+
+
+
+$(".botao").click(function(){
+	$('.results').text('TESTING FINALLY');
+});
+
+
 
 });
 
