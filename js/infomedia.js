ae65e6754f3d19280233162a26e383caa344c4a4
windsor80 <windsorjunior@hotmail.com>
2017-07-27 13:34:50 -0400

    updates

--- js/infomedia.js
+++ js/infomedia.js
@@ -133,7 +133,7 @@ function musicSearch() {
 		    displaysongsleft.push("<b>Artist:</b> " + musicarray[i].artist + "<br>");
 
 		    // Insert song links into the array
-		    displaysongsleft.push("<a href=" + musicarray[i].url +" target='_blank' class='btn btn-primary btn-xs news-btn'>More Info</a></td></tr></table><hr>");
+		    displaysongsleft.push("<a href=" + musicarray[i].url +" target='_blank' class='btn btn-primary btn-xs song-btn'>More Info</a></td></tr></table><hr>");
 
 
 		 };
@@ -151,7 +151,7 @@ function musicSearch() {
 		    displaysongsright.push("<b>Artist:</b> " + musicarray[i].artist + "<br>");
 
 		    // Insert song links into the array
-		    displaysongsright.push("<a href=" + musicarray[i].url +" target='_blank' class='btn btn-primary btn-xs news-btn'>More Info</a></td></tr></table><hr>");
+		    displaysongsright.push("<a href=" + musicarray[i].url +" target='_blank' class='btn btn-primary btn-xs song-btn'>More Info</a></td></tr></table><hr>");
 
 
 		 };
